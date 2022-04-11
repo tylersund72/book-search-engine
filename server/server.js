1944fd7a409d5b1cb5c2bba3ba7a71d35eea3cf7
Tyler <tylersundquist3@gmail.com>
2022-04-11 15:28:32 -0600

    stopping point, didnt realize was on master branch

--- server/server.js
+++ server/server.js
@@ -1,21 +1,24 @@
 const express = require("express");
-const path = require("path");
 const { ApolloServer } = require("apollo-server-express");
-const db = require("./config/connection");
-const routes = require("./routes");
+const path = require("path");
+
+
+// const routes = require("./routes");
 const { typeDefs, resolvers } = require("./schemas");
+
+const db = require("./config/connection");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-server.applyMiddleware({ app });
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
+
+server.applyMiddleware({ app });
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === "production") {
