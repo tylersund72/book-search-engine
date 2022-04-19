41e7c8a63b24e985666fced8725d25605f5ccea2
Tyler <tylersundquist3@gmail.com>
2022-04-19 15:52:47 -0600

    testing next()

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -31,6 +31,7 @@ module.exports = {
 
     // send to next endpoint
     next();
+    // return req;
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
